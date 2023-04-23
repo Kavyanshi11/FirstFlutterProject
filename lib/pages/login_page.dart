ac839ff622f14f4db98a3fdefdeaf0f7a9e271ea
kavyanshi11 <kavyanshiagarwal358@gmail.com>
2023-04-23 22:54:00 +0530

    day5

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,58 @@
+import "package:flutter/material.dart";
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset("assets/images/Login_image.png", fit: BoxFit.cover),
+          const SizedBox(
+            height: 20.0,
+          ),
+          const Text(
+            "Welcome",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: "Enter Username",
+                    labelText: "Username",
+                  ),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    hintText: "Enter Password",
+                    labelText: "Password",
+                  ),
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                ElevatedButton(
+                  child: const Text("login"),
+                  style: TextButton.styleFrom(),
+                  onPressed: () {
+                    const Text("hi Kavyanshi");
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
